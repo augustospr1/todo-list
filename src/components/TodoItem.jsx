53c521a5d5939e2125f6f0e3e3993ebab8576222
Augusto Sérgio <86673464+augustospr1@users.noreply.github.com>
2022-11-14 00:57:27 -0300

    Try to check if its a holiday or not

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,6 +12,7 @@ import { Paper } from "@mui/material";
 import EditTodoDialog from "./EditTodoDialog";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker';
 
 
 export default function TodoItem({ todo, deleteTodo, editTodo }) {
@@ -22,17 +23,20 @@ export default function TodoItem({ todo, deleteTodo, editTodo }) {
   };
 
   const [startDate, setStartDate] = useState(new Date());
-
   const getData = startDate.toISOString().substring(0,10);
-  const getHour = startDate.toISOString().substring(11, 19);
 
+  const [hour, setHour] = useState();
 
+  const url = 'https://date.nager.at/api/v3/publicholidays/2022/BR';
 
-  function mostraConsole() {
-    console.log(getData);
-  }
+    fetch(url).then(
+      (response) => {
+        return response.json()
+      }).then((date) => {
+        console.log( date.map( ({date, localName}) => ({date, localName}) ) );
+      })
 
-  mostraConsole();
+    console.log(`'` + getData + `'`);
 
   return (
     <>
@@ -53,13 +57,23 @@ export default function TodoItem({ todo, deleteTodo, editTodo }) {
             <ListItemText primary={todo.text} onClick={() => setOpenDialog(true)} />
           </ListItemButton>
         </ListItem>
-        <div style={{ padding: "0em 1em" }}>
-          <p style={{margin: "0em 0em 0.5em"}}>Data para fazer a tarefa</p>
-          <DatePicker style={{border: "none"}}
-          selected={startDate} 
-          onChange={(date) => setStartDate(date)} 
-          dateFormat="dd/MM/yyyy" />
-        </div>
+          <div style={{ padding: "0em 1em 0.5em" }} className="dateSelector">
+            <p style={{margin: "0em 0em 0.5em"}}>Dia e hora para fazer:</p>
+            <DatePicker style={{border: "none"}}
+            selected={startDate} 
+            onChange={(date) => setStartDate(date)} 
+            dateFormat="dd/MM/yyyy" />
+
+            {getData === 1 ? (
+              <div>
+                <p>feriado</p>
+              </div>
+            ) : <p>Dia normal</p>}
+            
+          </div>
+          <div style={{ padding: "0em 1em" }} className="hourSelector">
+            <TimePicker onChange={setHour} value={hour} format="h:m" clearAriaLabel="Clear value"/>
+          </div>
       </Paper>
     </>
   );
